19bc6b74e2d387d7c36d9cf02acb5f2e38288cda
Frank Carbonero <frankcarbonero@icloud.com>
2021-12-17 09:30:46 -0800

    pig

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react'
 import './App.css'
 import butcherPig from './assets/butcherPig.jpeg'
 
-class App extends Component{
-  constructor(props){
+class App extends Component {
+  constructor(props) {
     super(props)
     // the state object holds information that can be displayed to the user and updated throughout the program
     this.state = {
@@ -28,12 +28,49 @@ class App extends Component{
       // ACTION ITEM: use "currentWord" as a starting point for your code
       console.log("currentWord:", currentWord)
 
+
       let vowelsArray = currentWord.split("").filter(vowel => {
         return vowel === "a" || vowel === "e" || vowel === "i" || vowel === "o" || vowel === "u"
       })
       console.log("vowelsArray:", vowelsArray)
 
+      // 1st condition:
+      //    split the words at the first vowel and have the letters before the vowel, shift the end
+      //    of the same word.
+      //    shift the consonants to the end of the word when no vowel in index 0
+      //    add the <ay>  
+      //    pineapple with expected output :  ineapple-p-ay .... vowelsArray = [i, e, a, e,] 
+      //    
+      if (currentWord.indexOf(vowelsArray[0]) > 0) {
+        console.log("We made it in")
+        let copyCurrentWord = currentWord.slice(0, currentWord.length)
+        //  copyCurrentWord.splice(currentWord.indexOf(0)),
+      }
+      //    console.log("vowelsArray:", vowelsArray)
+      //    .splice(index, # of elements to remove, what you want to replace it with)
+      //      
+      //      
+      // 
+      // 
+      // 
+      // 2nd condition
+      //    when the vowel is in the index = 0 leave as is
+      //    add <way>
+      //    if the first character is a vowel, then take the whole word and 
+      // 
+      // 3rd condition
+      //    if <qu> shift, including the u, to the en of the word
+      //    add <ay>
+      //
+      //    filter
+      //  
+      // 4th condition 
+      //    no vowels but has the letter <y> , anything before the <y> gets shifted to right
+      // add <ay>
+      // 
       // your code here!
+
+
 
       // Remember: console.log is your friend :)
 
@@ -50,7 +87,7 @@ class App extends Component{
 
     // the setState method will take your information from "translatedWords" and update the state object that is displayed to the user
     // no need to change this method
-    this.setState({phraseTranslated: translatedWords})
+    this.setState({ phraseTranslated: translatedWords })
   }
 
   restartGame = () => {
@@ -72,7 +109,7 @@ class App extends Component{
   // no need to modify this method
   handleInput = (e) => {
     // this method takes the input and saves the value in this.state.phrase so we can use the input in our program
-    this.setState({phrase: e.target.value})
+    this.setState({ phrase: e.target.value })
   }
 
   render() {
